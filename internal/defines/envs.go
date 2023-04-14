19f2740f2563b2006ca85747b1537bc9eb561560
Lucas Bacelo <bacelolucas@gmail.com>
2023-04-14 11:14:15 -0300

    Added delete zone, implemented context to control go routines lifecycle, refactor to better readability.

--- internal/defines/envs.go
+++ internal/defines/envs.go
@@ -5,5 +5,6 @@ const (
 	EnvRedisPassword                = "REDIS_PASSWORD"
 	EnvRedisChannelGeoloc           = "REDIS_CHANNEL_GEOLOC"
 	EnvRedisChannelCreateDangerZone = "REDIS_CHANNEL_CREATE_ZONE"
+	EnvRedisChannelDeleteDangerZone = "REDIS_CHANNEL_DELETE_ZONE"
 	EnvRedisKeyDangerZone           = "REDIS_KEY_DANGER_ZONE"
 )
